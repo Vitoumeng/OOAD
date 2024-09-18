2f05ba2418267327ba356a64b92aa51387a449eb
MENG Vuthvitou <mengvuthvitou11@gmail.com>
2024-09-18 13:26:12 +0700

    Assigment 1 - Expense

---
+++ Expense/Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using expenselib;
+namespace ExpenseConsole;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            ExpenseTasks.Load("Expense.txt");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+        List<Expense> expenses = ExpenseTasks.Expenses;
+        Expense? max = ExpenseTasks.MaxAmountExpense;
+        double total = ExpenseTasks.AmountTotal;
+        Console.WriteLine($"{"No",-10} {"Amount",10} {"Descr",-30}");
+        string line = new string('-', 52);
+        Console.WriteLine(line);
+        expenses.ForEach(x => Console.WriteLine($"{x.No,-10} {x.Amount,10:C2} {x.Descr,-30}")); ;
+        Console.WriteLine(line);
+        Console.WriteLine($"Amount Total={total:c2}");
+        Console.WriteLine($"Max-amount expense> no:{max?.No}, descr:{max?.Descr}");
+    }
+}
---
+++ Expense/Library/Expense.cs
@@ -0,0 +1,7 @@
+﻿namespace expenselib;
+public class Expense
+{
+    public int No { get; set; } = 0;
+    public string Descr { get; set; } = "";
+    public double Amount { get; set; } = 0.0;
+}
